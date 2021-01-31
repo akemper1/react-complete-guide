8cb7ddb08005a460f0597feb79b1b2c29ce51b7d
Drew <andrewkemper1@gmail.com>
2021-01-31 17:08:29 -0500

    Initial React project

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Drew', age: 31 },
+      { name: 'Rachel', age: 28 },
+      { name: 'Steve', age: 55 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  };
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 32 },
+        { name: 'Rachel', age: 29 },
+        { name: 'Steve', age: 60 }
+      ]
+    });
+  };
+
+  nameChangedHandler = (event) =>
+  {
+    this.setState({
+      persons: [
+        { name: 'Drew', age: 32 },
+        { name: event.target.value, age: 29 },
+        { name: 'Steve', age: 60 }
+      ]
+    });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Switch Name
+        </button>
+        { 
+          this.state.showPersons ? 
+            <div>
+              <Person
+                name={this.state.persons[0].name}
+                age={this.state.persons[0].age}
+              />
+              <Person
+                name={this.state.persons[1].name}
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, 'Drew!')}
+                changed={this.nameChangedHandler}
+              >
+                My Hobbies: Sushi
+              </Person>
+              <Person
+                name={this.state.persons[2].name}
+                age={this.state.persons[2].age}
+              />
+            </div> : null
+        }
+      </div>
+    );
+  
+  }
+}
+
+export default App;
